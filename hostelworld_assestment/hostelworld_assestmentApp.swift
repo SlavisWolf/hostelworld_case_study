53b5f26b2e267fb6d31b70db699bd42f0e0e1226
Antonio <antoniojesus.ib@gmail.com>
2024-02-10 18:20:28 +0100

    Initial Commit

---
+++ hostelworld_assestment/hostelworld_assestmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  hostelworld_assestmentApp.swift
+//  hostelworld_assestment
+//
+//  Created by Antonio Jesús on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct hostelworld_assestmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
